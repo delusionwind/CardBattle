a9733a3a8c2ada767e7bb8b534024617c85340bf
Delusion <delusionwind@gmail.com>
2016-05-09 20:49:41 +0700

    add skill requirements and description

--- src/Skill.js
+++ src/Skill.js
@@ -1,23 +1,30 @@
 var Skill = cc.Sprite.extend({
-    ctor: function( phase, name, description ) {
+    ctor: function( phase, name, description, requirements ) {
         this._super();
         this.activated = false;
         this.selectLength = 0;
 
-        //this.initWithFile( 'res/images/logo.png' );
+        this.description = description;
+        this.requirements = requirements;
 
         var pos = this.getPosition();
         this.info = cc.LabelTTF.create( "" +phase + " " + name, 'ITC Stone Serif LT Italic', 15 );
         this.info.setPosition( new cc.Point( pos.x, pos.y ) );
         this.addChild( this.info );
+
+        this.info2 = cc.LabelTTF.create( requirements, 'ITC Stone Serif LT Italic', 12 );
+        this.info2.setPosition( new cc.Point( pos.x, pos.y - 14 ) );
+        this.addChild( this.info2 );
     },
     update: function( dt ) {
         var pos = this.getPosition();
         if ( this.activated == true ) {
             this.setPosition( new cc.Point( pos.x + this.selectLength, pos.y ) );
+            this.info2.setString( this.description );
             this.selectLength = 0;
         } else if ( this.activated == false ) {
             this.setPosition( new cc.Point( pos.x - this.selectLength, pos.y ) );
+            this.info2.setString( this.requirements );
             this.selectLength = 0;
         }
 
--- src/SkillList.js
+++ src/SkillList.js
@@ -9,13 +9,13 @@ var SkillList = cc.Node.extend({
         this.addChild( this.label );
 
         this.slot = new Array(10);
-        this.slot[0] = new Skill( "[MOV]", "Lightning FLASH", "SPEED+20" );
-        this.slot[1] = new Skill( "[ATK]", "Flame BLAST    ", "FIRE+8" );
-        this.slot[2] = new Skill( "[ATK]", "Stone PILLAR   ", "ROCK+8" );
-        this.slot[3] = new Skill( "[ATK]", "Thunder BOLT   ", "THUNDER+4" );
-        this.slot[4] = new Skill( "[ATK]", "Dark VOID      ", "ASTRAL+10" );
-        this.slot[5] = new Skill( "[DEF]", "Ice WALL       ", "ICE+5,ROCK+3" );
-        this.slot[6] = new Skill( "[DEF]", "Dark MATTER    ", "FIRE+6,ICE+6,THUNDER+6,ROCK+6," );
+        this.slot[0] = new Skill( "[MOV]", "Lightning FLASH", "SPEED+20", "THUNDER:1+" );
+        this.slot[1] = new Skill( "[ATK]", "Flame BLAST    ", "FIRE+8", "FIRE:4+" );
+        this.slot[2] = new Skill( "[ATK]", "Stone PILLAR   ", "ROCK+8", "ROCK:4+" );
+        this.slot[3] = new Skill( "[ATK]", "Thunder BOLT   ", "THUNDER+4", "THUNDER:3+" );
+        this.slot[4] = new Skill( "[ATK]", "Space VOID      ", "ASTRAL+10", "ASTRAL:4+" );
+        this.slot[5] = new Skill( "[DEF]", "Ice WALL       ", "ICE+5,ROCK+3", "ICE:3+" );
+        this.slot[6] = new Skill( "[DEF]", "Dark MATTER    ", "FIRE+6,ICE+6,THUNDER+6,ROCK+6,", "ASTRAL:5+" );
 
         for( var i = 0; i < this.slot.length; i++ ) {
             if ( this.slot[i] !== undefined ) {
